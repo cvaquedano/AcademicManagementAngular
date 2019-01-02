ad86fa2bde2a206449f5201b577ac6c26a23bdd7
cvaquedano <chvb2002@hotmail.com>
2019-01-02 08:22:36 -0600

    Create a shere module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import{FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { StudentsComponent } from './students/students.component';
 import { HttpClientModule } from '@angular/common/http';
-import { StudentDetailComponent } from './students/student-detail.component';
 import { RouterModule } from '@angular/router';
-import { StudentDetailGuard } from './students/student-detail.guard';
 import { StudentModule } from './students/student.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    StudentsComponent,
    
-    StudentDetailComponent,
   ],
   
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
+   
     RouterModule.forRoot([
-      {path:'students',component: StudentsComponent},
-      {path:'students/:id',
-      canActivate:[StudentDetailGuard],
-      component: StudentDetailComponent},
+     
       {path:'welcome',component: WelcomeComponent},
       {path:'',redirectTo: 'welcome',pathMatch:'full'},
       {path:'**',redirectTo: 'welcome',pathMatch:'full'}
--- src/app/students/student.module.ts
+++ src/app/students/student.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { StudentsComponent } from './students.component';
+import { StudentDetailComponent } from './student-detail.component';
+import { RouterModule } from '@angular/router';
+import { StudentDetailGuard } from './student-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [ 
+    StudentsComponent,   
+    StudentDetailComponent,
+  ],
   imports: [
-    CommonModule
+
+    RouterModule.forChild([
+      {path:'students',component: StudentsComponent},
+      {path:'students/:id',
+      canActivate:[StudentDetailGuard],
+      component: StudentDetailComponent},]
+    ),
+    SharedModule
   ]
 })
 export class StudentModule { }
